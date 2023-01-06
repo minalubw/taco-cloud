24edd31e992252319745829b947fbe0658890610
minalubw <minalubiresaw@gmail.com>
2023-01-06 17:15:28 -0600

    connected to sql

--- src/main/java/tacos/domain/Taco.java
+++ src/main/java/tacos/domain/Taco.java
@@ -1,10 +1,20 @@
 package tacos.domain;
 
+import java.util.Date;
 import java.util.List;
 import lombok.Data;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 public class Taco {
+	@NotNull
+	@Size(min=5, message="name must be atleast 5 chars long")
 	private String name;
+	
+	@Size(min=1, message="You must select at least one ingredient")
 	private List<String> ingredients;
+	
+	private Long id;
+	private Date createdAt;
 }
